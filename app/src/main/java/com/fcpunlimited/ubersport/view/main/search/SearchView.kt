f6b62f85624738cee8085ebc613c0b7c4ae81f54
a.krotkov <a.krotkov@mob-edu.ru>
2018-10-03 18:55:28 +0300

    fix

--- app/src/main/java/com/fcpunlimited/ubersport/view/main/search/SearchView.kt
+++ app/src/main/java/com/fcpunlimited/ubersport/view/main/search/SearchView.kt
@@ -4,7 +4,8 @@ import com.arellomobile.mvp.MvpView
 import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 
-@StateStrategyType(AddToEndSingleStrategy::class)
 interface SearchView : MvpView {
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
     fun onSwipeRefresh(isRefreshing: Boolean)
 }
