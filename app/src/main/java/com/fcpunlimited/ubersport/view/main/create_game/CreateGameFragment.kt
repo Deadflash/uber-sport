085359c2611aca77b2c2ed6346fe16127d9375b5
a.krotkov <a.krotkov@mob-edu.ru>
2019-08-14 11:14:10 +0300

    create game fragment

---
+++ app/src/main/java/com/fcpunlimited/ubersport/view/main/create_game/CreateGameFragment.kt
@@ -0,0 +1,60 @@
+package com.fcpunlimited.ubersport.view.main.create_game
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.preference.PreferenceFragmentCompat
+import com.arellomobile.mvp.presenter.InjectPresenter
+import com.arellomobile.mvp.presenter.PresenterType
+import com.arellomobile.mvp.presenter.ProvidePresenter
+import com.fcpunlimited.ubersport.R
+import com.fcpunlimited.ubersport.utils.Constants.CREATE_GAME_FRAGMENT_TAG
+import com.fcpunlimited.ubersport.view.BaseMvpFragment
+import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.toast
+
+class CreateGameFragment : PreferenceFragmentCompat(), CreateGameView {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.create_game_preferences, rootKey)
+
+    }
+
+    companion object {
+        const val CREATE_GAME_FRAGMENT_PRESENTER = "createGameFragmentPresenter"
+    }
+
+//    private val gamesLiveDataContainer: GamesLiveDataContainer by inject()
+//    private val gameModel: GameModel by inject()
+
+    @InjectPresenter(type = PresenterType.GLOBAL, tag = CREATE_GAME_FRAGMENT_PRESENTER)
+    lateinit var presenter: CreateGamePresenter
+
+    @ProvidePresenter(type = PresenterType.GLOBAL, tag = CREATE_GAME_FRAGMENT_PRESENTER)
+    fun providePresenter() = CreateGamePresenter()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        addPreferencesFromResource()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun showMessage(message: String) {
+        context?.runOnUiThread { toast(message) }
+    }
+
+//    override fun getFragmentLayout(): Int = R.layout.fragment_create_game
+//
+//    override fun getFragmentTag(): String = CREATE_GAME_FRAGMENT_TAG
+}
---
+++ app/src/main/java/com/fcpunlimited/ubersport/view/main/create_game/CreateGamePresenter.kt
@@ -0,0 +1,7 @@
+package com.fcpunlimited.ubersport.view.main.create_game
+
+import com.arellomobile.mvp.InjectViewState
+
+@InjectViewState
+class CreateGamePresenter {
+}
